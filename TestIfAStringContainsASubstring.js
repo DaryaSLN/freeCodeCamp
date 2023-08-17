be2ef17299d50284c376cb66133b34e515a76e9b
Darya <darya.slinkova@gmail.com>
2023-08-17 14:54:27 +0300

    add TestIfAStringContainsASubstring

--- TestIfAStringContainsASubstring.js
+++ TestIfAStringContainsASubstring.js
@@ -1,10 +1,11 @@
 // DECRIPTION:
-// Within tests/1_unit-tests.js under the test labelled #14 in the Strings suite, 
-// change each assert to either assert.include or assert.notInclude to make the test 
-// pass (should evaluate to true). Do not alter the arguments passed to the asserts.
+// Within tests/1_unit-tests.js under the test labelled #15 in the Strings suite, 
+// change each assert to either assert.match or assert.notMatch to make the test pass 
+// (should evaluate to true). Do not alter the arguments passed to the asserts.
 
-// #14
-    test('String #include, #notInclude', function () {
-      assert.include('Arrow', 'row', "'Arrow' contains 'row'");
-      assert.notInclude('dart', 'queue', "But 'dart' doesn't contain 'queue'");
+// #15
+    test('#match, #notMatch', function () {
+      const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
+      assert.match(formatPeople('John Doe', 35), regex);
+      assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex);
     });
